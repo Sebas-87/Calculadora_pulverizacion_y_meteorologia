9b9f058644ec354f8e28ec33a742b748f9fdb782
Sebastian Rollan <sebarollan@gmail.com>
2023-08-07 18:30:41 -0300

    create new options, changes in js and fix errors

--- main.js
+++ main.js
@@ -1,102 +1,201 @@
-      // Definición de objeto y funciones
-      const Agroquimico = {
-        nombre: "",
-        superficie: 0,
-        dosis: 0,
-        unidad: "",
-        cantidadAgroquimicos: 0,
-        setNombre: function (nombre) {
-            this.nombre = nombre;
-        },
-        setSuperficie: function (superficie) {
-            if (!isNaN(superficie)) {
-                this.superficie = superficie;
-            }
-        },
-        setDosis: function (dosis) {
-            if (!isNaN(dosis)) {
-                this.dosis = dosis;
-            }
-        },
-        setUnidad: function (unidad) {
-            this.unidad = unidad;
-        },
-        calcularCantidadAgroquimicos: function () {
-            this.cantidadAgroquimicos = this.superficie * this.dosis;
-            if (Number.isInteger(this.cantidadAgroquimicos)) {
-                // Si la cantidad es un número entero, no mostramos decimales
-                this.cantidadAgroquimicos = this.cantidadAgroquimicos.toFixed(0);
-            } else {
-                // Si la cantidad tiene decimales, mostramos solo dos decimales
-                this.cantidadAgroquimicos = this.cantidadAgroquimicos.toFixed(2);
-                // Eliminamos los ceros finales si el tercer decimal es cero
-                this.cantidadAgroquimicos = parseFloat(this.cantidadAgroquimicos);
-            }
-            this.cantidadAgroquimicos += " " + this.unidad;
-        },
-    };
+const tiposFormulacion = {
+    "Bolsas hidrosolubles": 1,
+    "Polvos mojables (WP)": 2,
+    "Granulados dispersables (WG)": 3,
+    "Granulados solubles (SG)": 4,
+    "Dispersiones oleosas (OD)": 5,
+    "Suspensiones concentradas (SC)": 6,
+    "Formulaciones (ZC)": 7,
+    "Suspensiones de encapsulados (CS)": 8,
+    "Suspo-emulsiones (SE)": 9,
+    "Emulsiones de aceite en agua (EW)": 10,
+    "Concentrados emulsionables (EC)": 11,
+    "Concentrados solubles (SL)": 12,
+    "Otros adyuvantes como aceites/surfactantes": 13,
+    "Micro nutrientes/fertilizantes foliares": 14,
+};
 
-    let calculosAgroquimicos = []; // Array para almacenar los cálculos de agroquímicos
+let ordenMezcla = 1; // Contador de orden de mezcla
 
-    function calcularAgroquimicos() {
-        let agroquimico = Object.create(Agroquimico);
+function agregarAgroquimico() {
+    const container = document.getElementById("agroquimicosContainer");
 
-        let nombreInput = document.getElementById("nombre");
-        let superficieInput = document.getElementById("superficie");
-        let dosisInput = document.getElementById("dosis");
-        let unidadInput = document.getElementById("unidad");
+    const agroquimicoDiv = document.createElement("div");
+    agroquimicoDiv.className = "agroquimico";
 
-        let nombre = nombreInput.value;
-        let superficie = parseFloat(superficieInput.value);
-        let dosis = parseFloat(dosisInput.value);
-        let unidad = unidadInput.value;
+    const nombreInput = document.createElement("input");
+    nombreInput.type = "text";
+    nombreInput.placeholder = "Nombre del agroquímico";
+    agroquimicoDiv.appendChild(nombreInput);
 
-        if (isNaN(superficie) || isNaN(dosis)) {
-            alert("Valor(es) ingresado(s) no válido(s). Por favor, ingrese valores numéricos válidos.");
-            return;
-        }
-
-        agroquimico.setNombre(nombre);
-        agroquimico.setSuperficie(superficie);
-        agroquimico.setDosis(dosis);
-        agroquimico.setUnidad(unidad);
-        agroquimico.calcularCantidadAgroquimicos();
-
-        calculosAgroquimicos.push(agroquimico); // Agregar el cálculo al array de calculosAgroquimicos
-
-        // Mostrar el resultado en la consola
-        console.log("Cantidad de agroquímicos a utilizar para " + agroquimico.nombre + ": " + agroquimico.cantidadAgroquimicos);
-
-        // Mostrar el resultado en la pantalla
-        let resultadosTable = document.getElementById("resultados");
-        let newRow = resultadosTable.insertRow(-1);
-
-        let calculoCell = newRow.insertCell(0);
-        calculoCell.textContent = calculosAgroquimicos.length;
-
-        let nombreCell = newRow.insertCell(1);
-        nombreCell.textContent = agroquimico.nombre;
-
-        let superficieCell = newRow.insertCell(2);
-        superficieCell.textContent = agroquimico.superficie;
-
-        let dosisCell = newRow.insertCell(3);
-        dosisCell.textContent = agroquimico.dosis;
-
-        let unidadCell = newRow.insertCell(4);
-        unidadCell.textContent = agroquimico.unidad;
-
-        let cantidadAgroquimicosCell = newRow.insertCell(5);
-        cantidadAgroquimicosCell.textContent = agroquimico.cantidadAgroquimicos;
-
-        let borrarButton = document.createElement("button");
-        borrarButton.textContent = "Borrar";
-        borrarButton.addEventListener("click", function () {
-            let fila = this.parentNode.parentNode;
-            fila.parentNode.removeChild(fila);
-            calculosAgroquimicos.splice(fila.rowIndex - 1, 1);
-        });
-
-        let borrarCell = newRow.insertCell(6);
-        borrarCell.appendChild(borrarButton);
+    const tipoSelect = document.createElement("select");
+    for (const tipo in tiposFormulacion) {
+        const option = document.createElement("option");
+        option.value = tipo;
+        option.textContent = tipo;
+        tipoSelect.appendChild(option);
     }
+    agroquimicoDiv.appendChild(tipoSelect);
+
+    const cantidadInput = document.createElement("input");
+    cantidadInput.type = "number";
+    cantidadInput.placeholder = "Cantidad";
+    agroquimicoDiv.appendChild(cantidadInput);
+
+    const unidadSelect = document.createElement("select");
+    unidadSelect.id = "unidadDosis";
+    const litrosOption = document.createElement("option");
+    litrosOption.value = "litros";
+    litrosOption.textContent = "Litros";
+    unidadSelect.appendChild(litrosOption);
+    const kilogramosOption = document.createElement("option");
+    kilogramosOption.value = "kilogramos";
+    kilogramosOption.textContent = "Kilogramos";
+    unidadSelect.appendChild(kilogramosOption);
+    agroquimicoDiv.appendChild(unidadSelect);
+
+    const eliminarButton = document.createElement("button");
+    eliminarButton.textContent = "Eliminar";
+    eliminarButton.onclick = function () {
+        agroquimicoDiv.remove(); // Cambio en esta línea para evitar eliminar los cálculos guardados.
+    };
+    agroquimicoDiv.appendChild(eliminarButton);
+
+    container.appendChild(agroquimicoDiv);
+}
+
+function calcularCantidadCaldo() {
+    const hectareasTotales = parseFloat(document.getElementById("hectareasTotales").value);
+    const litrosPorHectarea = parseFloat(document.getElementById("litrosPorHectarea").value);
+
+    if (!hectareasTotales || !litrosPorHectarea) {
+        alert("Por favor, ingrese valores válidos para las hectáreas totales y los litros por hectárea.");
+        return;
+    }
+
+    const cantidadCaldo = hectareasTotales * litrosPorHectarea;
+
+    const caldoContainer = document.getElementById("caldoContainer");
+    caldoContainer.innerHTML = `<p>Cantidad de Caldo Necesaria: ${cantidadCaldo.toFixed(2)} litros</p>`;
+}
+
+function calcularPulverizacion() {
+    const hectareasTotales = parseFloat(document.getElementById("hectareasTotales").value);
+
+    if (!hectareasTotales) {
+        alert("Por favor, ingrese un valor válido para las hectáreas totales.");
+        return;
+    }
+
+    const agroquimicosDivs = document.querySelectorAll("#agroquimicosContainer .agroquimico");
+
+    const agroquimicos = [];
+    for (const agroquimicoDiv of agroquimicosDivs) {
+        const agroquimico = {
+            nombre: agroquimicoDiv.querySelector("input[type='text']").value,
+            formulacion: agroquimicoDiv.querySelector("select").value,
+            cantidad: parseFloat(agroquimicoDiv.querySelector("input[type='number']").value),
+            unidad: agroquimicoDiv.querySelector("#unidadDosis").value,
+        };
+
+        agroquimicos.push(agroquimico);
+    }
+
+    agroquimicos.sort((a, b) => tiposFormulacion[a.formulacion] - tiposFormulacion[b.formulacion]);
+
+    const resultadosContainer = document.getElementById("resultadosContainer");
+    resultadosContainer.innerHTML = "";
+
+    for (let i = 0; i < agroquimicos.length; i++) {
+        const agroquimico = agroquimicos[i];
+
+        const cantidadTotal = agroquimico.unidad === "litros"
+            ? (agroquimico.cantidad * hectareasTotales).toFixed(2)
+            : (agroquimico.cantidad * hectareasTotales).toFixed(2);
+
+        const resultadoDiv = document.createElement("div");
+        resultadoDiv.innerHTML = `
+        <p><strong>Orden de Mezcla en Tanque:</strong> ${ordenMezcla++}</p>
+        <p><strong>Agroquímico:</strong> ${agroquimico.nombre}</p>
+        <p><strong>Formulación:</strong> ${agroquimico.formulacion}</p>
+        <p><strong>Cantidad total:</strong> ${cantidadTotal.replace('.', ',')} ${agroquimico.unidad}</p>
+        <hr>
+      `;
+        resultadosContainer.appendChild(resultadoDiv);
+    }
+}
+
+function guardarPulverizacion() {
+    const cantidadCaldo = document.getElementById("caldoContainer").innerHTML;
+    const resultadosPulverizacion = document.getElementById("resultadosContainer").innerHTML;
+
+    localStorage.setItem("cantidadCaldo", cantidadCaldo);
+    localStorage.setItem("resultadoPulverizacion", resultadosPulverizacion);
+
+    alert("Cálculo guardado correctamente.");
+}
+
+function recuperarPulverizacionGuardada() {
+    const cantidadCaldoGuardada = localStorage.getItem("cantidadCaldo");
+    const resultadosPulverizacionGuardados = localStorage.getItem("resultadoPulverizacion");
+
+    if (cantidadCaldoGuardada) {
+        document.getElementById("caldoContainer").innerHTML = cantidadCaldoGuardada;
+    }
+
+    if (resultadosPulverizacionGuardados) {
+        document.getElementById("resultadosContainer").innerHTML = resultadosPulverizacionGuardados;
+    }
+}
+
+function resetearCalculador() {
+    document.getElementById("hectareasTotales").value = "";
+    document.getElementById("litrosPorHectarea").value = "";
+
+    const agroquimicosDivs = document.querySelectorAll("#agroquimicosContainer .agroquimico");
+    for (const agroquimicoDiv of agroquimicosDivs) {
+        document.getElementById("agroquimicosContainer").removeChild(agroquimicoDiv);
+    }
+
+    ordenMezcla = 1;
+
+    const caldoContainer = document.getElementById("caldoContainer");
+    caldoContainer.innerHTML = ""; // Limpiar resultados de la pantalla
+
+    const resultadosContainer = document.getElementById("resultadosContainer");
+    resultadosContainer.innerHTML = ""; // Limpiar resultados de la pantalla
+}
+
+function mostrarCalculoGuardado() {
+    const cantidadCaldoGuardada = localStorage.getItem("cantidadCaldo");
+    const resultadosPulverizacionGuardados = localStorage.getItem("resultadoPulverizacion");
+
+    const calculoGuardadoContainer = document.getElementById("calculoGuardadoContainer");
+    calculoGuardadoContainer.innerHTML = "";
+
+    if (cantidadCaldoGuardada) {
+        const cantidadCaldoDiv = document.createElement("div");
+        cantidadCaldoDiv.innerHTML = `<p>Cantidad de Caldo Necesaria (Guardada): ${cantidadCaldoGuardada}</p>`;
+        calculoGuardadoContainer.appendChild(cantidadCaldoDiv);
+    }
+
+    if (resultadosPulverizacionGuardados) {
+        const resultadosPulverizacionDiv = document.createElement("div");
+        resultadosPulverizacionDiv.innerHTML = `<p>Resultados de Pulverización (Guardados):</p>${resultadosPulverizacionGuardados}`;
+        calculoGuardadoContainer.appendChild(resultadosPulverizacionDiv);
+    }
+}
+
+function ocultarGuardado() {
+    const calculoGuardadoContainer = document.getElementById("calculoGuardadoContainer");
+    calculoGuardadoContainer.innerHTML = "";
+}
+
+function borrarLoGuardado() {
+    // Esta función borra los datos guardados en la memoria.
+    localStorage.clear();
+
+    // Actualiza la pantalla para mostrar que los datos se han borrado.
+    document.getElementById("caldoContainer").innerHTML = "";
+    document.getElementById("resultadosContainer").innerHTML = "";
+}
